2c07bef8f47a455d6176e186c32a7e18cc8d8588
anandkr47 <anandkumar.trident@gmail.com>
2023-11-12 02:23:48 +0530

    login updates

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -92,7 +92,7 @@ export default function Login() {
         </div>
 
         <div className="w-full md:w-2/5 bg-black text-white rounded-tr-2xl rounded-br-2xl md:py-10 px-4 md:px-12">
-          <h2 className="text-xl md:text-3xl font-bold mb-2">Hello Friend!</h2>
+          <h2 className="text-xl md:text-3xl font-bold mb-2">Lets Search gifs!</h2>
           <div className="border-2 w-6 md:w-10 border-white inline-block mb-2 md:ml-16"></div>
           <p className="mb-6 md:mb-10">Fill up the personal info and start up your journey.</p>
           <Link href="/signup">
